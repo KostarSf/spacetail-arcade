4cb8886794a25230e6923912c55e9e88a1afba5f
Maksim Peskov <kostarsf@gmail.com>
2024-06-14 00:27:58 +0300

    ship ECS test

---
+++ src/ecs/ship/ship-ecs.ts
@@ -0,0 +1,64 @@
+import {
+    Component,
+    MotionComponent,
+    Query,
+    System,
+    SystemPriority,
+    SystemType,
+    TransformComponent,
+    Vector,
+    World,
+} from "excalibur";
+
+export class ShipComponent extends Component {
+    accelerated: boolean = false;
+
+    rotationTarget: Vector | null = null;
+
+    readonly dependencies = [TransformComponent, MotionComponent];
+
+    constructor() {
+        super();
+    }
+}
+
+export class ShipSystem extends System {
+    public systemType: SystemType = SystemType.Update;
+    public priority: number = SystemPriority.Average;
+
+    private query: Query<
+        | typeof ShipComponent
+        | typeof TransformComponent
+        | typeof MotionComponent
+    >;
+
+    constructor(world: World) {
+        super();
+        this.query = world.query([ShipComponent]);
+    }
+
+    update(elapsedMs: number): void {
+        let transform: TransformComponent;
+        let motion: MotionComponent;
+        let ship: ShipComponent;
+
+        const entities = this.query.entities;
+        for (let i = 0; i < entities.length; i++) {
+            transform = entities[i].get(TransformComponent);
+            motion = entities[i].get(MotionComponent);
+            ship = entities[i].get(ShipComponent);
+
+            if (ship.rotationTarget) {
+                transform.rotation = ship.rotationTarget.toAngle();
+            }
+
+            if (ship.accelerated) {
+                motion.vel.addEqual(
+                    Vector.fromAngle(transform.rotation).scale(100 / elapsedMs)
+                );
+            } else {
+                motion.vel.scaleEqual(0.995);
+            }
+        }
+    }
+}
--- src/player.ts
+++ src/player.ts
@@ -1,19 +1,32 @@
-import { Actor, vec } from "excalibur";
+import { Actor, Engine, vec } from "excalibur";
 import { Resources } from "./resources";
+import { ShipComponent } from "./ecs/ship/ship-ecs";
 
 export class Player extends Actor {
-  constructor() {
-    super({
-      pos: vec(150, 150),
-      width: 100,
-      height: 100
-    });
-  }
+    constructor() {
+        super({
+            pos: vec(150, 150),
+            width: 100,
+            height: 100,
+        });
 
-  onInitialize() {
-    this.graphics.add(Resources.Sword.toSprite());
-    this.on('pointerup', () => {
-      alert('yo');
-    });
-  }
+        this.addComponent(new ShipComponent());
+    }
+
+    onInitialize(engine: Engine): void {
+        this.graphics.add(Resources.Sword.toSprite());
+        engine.input.pointers.on("move", (e) => {
+            this.ship.rotationTarget = e.worldPos;
+        });
+        engine.input.pointers.primary.on("down", () => {
+            this.ship.accelerated = true;
+        });
+        engine.input.pointers.primary.on("up", () => {
+            this.ship.accelerated = false;
+        });
+    }
+
+    get ship() {
+        return this.get(ShipComponent);
+    }
 }
